d76e81d2e7447d25d2dbbc3f7346133d0f45c8e1
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-06-22 23:17:22 +0100

    Implemented uploading of images

--- dotastro_hack_submission/add_files.py
+++ dotastro_hack_submission/add_files.py
@@ -1,7 +1,7 @@
 from github import InputGitTreeElement
 
 
-def add_file(repo, branch, message, filename, contents):
+def add_files(repo, branch, message, files):
     """
     Add a new file to a new branch in a GitHub repo
     """
@@ -19,8 +19,10 @@ def add_file(repo, branch, message, filename, contents):
                                               sha=element.sha))
 
     # We now make a blob with the new file contents and add it to the tree
-    blob = repo.create_git_blob(content=contents, encoding="utf-8")
-    tree_input.append(InputGitTreeElement(filename, "100644", "blob", sha=blob.sha))
+    for filename, content in files.items():
+        content, encoding = content
+        blob = repo.create_git_blob(content=content, encoding=encoding)
+        tree_input.append(InputGitTreeElement(filename, "100644", "blob", sha=blob.sha))
 
     # We make a new tree, commit, and branch
     tree = repo.create_git_tree(tree=tree_input)
@@ -40,19 +42,20 @@ doi: {doi}
 images: {images}
 orcid: {orcid}
 """
-def make_file_contents(formdct):
-    title = formdct['title']
-    desc = formdct['description']
-    src = formdct['source_url']
-    live = formdct['live_url']
-    doi = formdct['doi']
-    email = formdct['email']
-    images = formdct['pic']
+def make_file_contents(request, image_filename):
 
-    creators = [s.strip() for s in formdct['creators'].split(',')]
+    title = request.form['title']
+    desc = request.form['description']
+    src = request.form['source_url']
+    live = request.form['live_url']
+    doi = request.form['doi']
+    email = request.form['email']
+    images = image_filename
+
+    creators = [s.strip() for s in request.form['creators'].split(',')]
     creators = '\n    - ' + '\n    - '.join(creators)
 
-    orcid = [s.strip() for s in formdct['orcid'].split(',')]
+    orcid = [s.strip() for s in request.form['orcid'].split(',')]
     orcid = '\n    - ' + '\n    - '.join(orcid)
 
     return YML_TEMPLATE.format(**locals())
